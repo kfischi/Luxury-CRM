7a4c3a1018535f6d9b734542483c452cd91874e0
kfischi <kfir.biz@gmail.com>
2025-08-11 23:57:38 +0300

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,1 +1,11 @@
-
+// Global app settings (6 stages)
+window.CRM_SETTINGS = {
+  CLIENT_STATUSES: {
+    new_lead: 'ליד חדש',
+    negotiation: 'משא ומתן',
+    pending: 'מחכה לאישור',
+    booked: 'הזמנה פעילה',
+    completed: 'הושלם',
+    cancelled: 'בוטל'
+  }
+};
